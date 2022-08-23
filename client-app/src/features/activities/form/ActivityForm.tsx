b5a43c997df4fc3e70ca68ddbd83b174787902c7
DeathsAngel <lmansgreat@gmail.com>
2022-08-23 09:04:11 -0400

    End of section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,7 +12,7 @@ import MyTextArea from "./MyTextArea";
 import MySelectInput from "./MySelectInput";
 import { categoryOptions } from "../../../app/common/options/CategoryOptions";
 import MyDateInput from "./MyDateInput";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -21,21 +21,12 @@ export default observer(function ActivityForm() {
   const {
     createActivity,
     updateActivity,
-    loading,
     loadActivity,
     loadingInitial,
   } = activityStore;
   const { id } = useParams<{ id: string }>();
 
-  const [activity, setActivity] = useState<Activity>({
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: null,
-    city: "",
-    venue: "",
-  });
+  const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
   const validationSchema = Yup.object({
     title: Yup.string().required('The activity title is required!'),
@@ -47,11 +38,11 @@ export default observer(function ActivityForm() {
   })
 
   useEffect(() => {
-    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+    if (id) loadActivity(id).then((activity) => setActivity(new ActivityFormValues(activity)));
   }, [id, loadActivity]);
 
-  function handleFormSubmit(activity: Activity) {
-      if(activity.id.length === 0){
+  function handleFormSubmit(activity: ActivityFormValues) {
+      if(!activity.id){
           let newActivity = {...activity, id: uuid()};
           createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
       } else {
@@ -81,7 +72,7 @@ export default observer(function ActivityForm() {
             <Header content='Location Details' sub color='teal' />
             <MyTextInput placeholder="City" name="city" />
             <MyTextInput placeholder="Venue" name="venue" />
-            <Button disabled={isSubmitting || !dirty || !isValid} loading={loading} floated="right" positive type="submit" content="Submit" />
+            <Button disabled={isSubmitting || !dirty || !isValid} loading={isSubmitting} floated="right" positive type="submit" content="Submit" />
             <Button as={Link} to="/activities" floated="right" type="button" content="Cancel" />
           </Form>
         )}
