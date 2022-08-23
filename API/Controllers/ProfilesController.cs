6ae84a88b6c0734b7cf6586d375c805157061743
DeathsAngel <lmansgreat@gmail.com>
2022-08-23 16:56:04 -0400

    End of section 18

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,17 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Application.Profiles;
-
+using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
-    public class ProfilesController : BaseApiController
-    {
-        [HttpGet("{username}")]
-        public async Task<IActionResult> GetProfile(string username){
-            return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
-        }
-    }
+ public class ProfilesController : BaseApiController
+ {
+ [HttpGet("{username}")]
+ public async Task<IActionResult> GetProfile(string username)
+ {
+ return HandleResult(await Mediator.Send(new Details.Query{Username 
+= username}));
+ }
+ [HttpPut]
+ public async Task<IActionResult> Edit(Edit.Command command)
+ {
+ return HandleResult(await Mediator.Send(command));
+ }
+ }
 }
